2598221961ac9dca67538748be0a3efb3ecf4c3b
James Hooper <jameshooperdesigns@gmail.com>
2021-11-30 00:00:17 -0700

    Add JS that adds a blinking cursor to the Alexa section

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function toggleDropDown() {
   document.getElementById('form-section').classList.toggle('hidden');
 }
 
+const words = ["Jesse.", "A Father.", "A Husband.", "A Developer."]
+
+let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1 })
