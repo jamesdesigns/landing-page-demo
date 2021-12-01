55ed4c1c4ecaa9bffbf7781304a3eb108338430d
James Hooper <jameshooperdesigns@gmail.com>
2021-11-30 23:47:50 -0700

    Add to the JS for the Alexa animation

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleDropDown() {
 const words = ["Jesse.", "A Father.", "A Husband.", "A Developer."]
 
 let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1 })
+
+let boxTl = gsap.timeline()
+boxTl.to('.box', {duration: 1, width: "17vw", delay: 0.5, ease: "power4.inOut" })
+  .from('.hi', {duration: 1, y:"7vw", ease:"power3.out"})
+  .to('.box', {duration: 1, height:"7vw", ease:"elastic.out" })
+
+  let masterTl = gsap.timeline({repeat: -1})
+
+  words.forEach(word => {
+    let tl = gsap.timeline({repeat: 1, yoyo: true})
+    tl.to('.text', {duration: 1, text: word})
+    masterTl.add(tl)
+  })
