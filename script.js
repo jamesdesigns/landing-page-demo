314e3de871e6230a6dd845c1645086cc36653726
James Hooper <jameshooperdesigns@gmail.com>
2021-12-01 17:25:28 -0700

    Adjust the JS settings of the animation

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const words = ["start ResidentPortal.", "what was my last payment?", "call my ag
 let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: 1 })
 
 let boxTl = gsap.timeline()
-boxTl.to('.box', {duration: 1, width: "17vw", delay: 0.5, ease: "power4.inOut" })
-  .from('.hi', {duration: 1, y:"7vw", ease:"power3.out"})
-  .to('.box', {duration: 1, height:"7vw", ease:"elastic.out" })
+boxTl.to('.box', {duration: 1.6, width: "17vw", delay: 0.5, ease: "power4.inOut" })
+  .from('.text', {duration: 1.6, y:"7vw", ease:"power3.out"})
+  .to('.box', {duration: 1.6, height:"7vw", ease:"elastic.out" })
 
   let masterTl = gsap.timeline({repeat: -1})
 
   words.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true})
-    tl.to('.text', {duration: 1, text: word})
+    tl.to('.text', {duration: 1.6, text: word})
     masterTl.add(tl)
   })
