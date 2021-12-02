47b4912158f1c1f35b950f755b6133fa21214385
James Hooper <jameshooperdesigns@gmail.com>
2021-12-01 22:49:47 -0700

    Slow down the animation

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function toggleDropDown() {
   document.getElementById('form-section').classList.toggle('hidden');
 }
 
-const words = ["Start ResidentPortal.", "What was my last payment?"]
+const words = ["Start ResidentPortal.", "What was my last payment?", "When is my next appointment?", "Turn off the lights."]
 
 let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: 1 })
 
 let boxTl = gsap.timeline()
-boxTl.to('.box', {duration: 1.6, width: "17vw", delay: 0.5, ease: "power4.inOut" })
-  .from('.text', {duration: 1.6, y:"7vw", ease:"power3.out"})
-  .to('.box', {duration: 1.6, height:"7vw", ease:"elastic.out" })
+boxTl.to('.box', {duration: 1.8, width: "17vw", delay: 0.5, ease: "power4.inOut" })
+  .from('.text', {duration: 1.8, y:"7vw", ease:"power3.out"})
+  .to('.box', {duration: 1.8, height:"7vw", ease:"elastic.out" })
 
-  let masterTl = gsap.timeline({repeat: 0})
+  let masterTl = gsap.timeline({repeat: -1})
 
   words.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true})
-    tl.to('.text', {duration: 1.6, text: word})
+    tl.to('.text', {duration: 1.8, text: word})
     masterTl.add(tl)
   })
