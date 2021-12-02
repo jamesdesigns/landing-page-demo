3d08f5b9f63cf4b8b9f692caf0da7d3060957f23
James Hooper <jameshooperdesigns@gmail.com>
2021-12-01 17:28:35 -0700

    Minimize content that will animate

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleDropDown() {
   document.getElementById('form-section').classList.toggle('hidden');
 }
 
-const words = ["start ResidentPortal.", "what was my last payment?", "call my agent.", "what times is it?"]
+const words = ["Start ResidentPortal.", "What was my last payment?"]
 
 let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: 1 })
 
@@ -12,7 +12,7 @@ boxTl.to('.box', {duration: 1.6, width: "17vw", delay: 0.5, ease: "power4.inOut"
   .from('.text', {duration: 1.6, y:"7vw", ease:"power3.out"})
   .to('.box', {duration: 1.6, height:"7vw", ease:"elastic.out" })
 
-  let masterTl = gsap.timeline({repeat: -1})
+  let masterTl = gsap.timeline({repeat: 0})
 
   words.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true})
